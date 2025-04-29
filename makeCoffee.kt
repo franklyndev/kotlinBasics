58f91a61c497d139914b94402d80a09d4c5cce42
FranklynSantos <franklynsantos451@gmail.com>
2025-04-29 13:22:12 -0300

    makeCoffee

--- makeCoffee.kt
+++ makeCoffee.kt
@@ -1,25 +1,25 @@
 package com.example.activity
 
 
-
 fun main(){
     // we call the function
+    makeCoffee()
+    makeCoffee()
+    makeCoffee()
+
+}
+// we define the function
+fun makeCoffee(){
     println("Please enter your name: ")
     var name = readln()
     println("How many spoons of sugar do you want?")
     var sugarCount = readln().toInt()
-
-    makeCoffee(sugarCount, name)
-
-}
-// we define the function
-fun makeCoffee(sugarCount : Int, name : String){
 
     if (sugarCount < 1) {
         println("$name, you have not added any spoon of sugar")
     }else if (sugarCount > 1) {
         println("Coffee with $sugarCount spoons of sugar for $name")
     }else{
-        println("Coffee with $sugarCount spoon of sugar for $name")
-    }
+        println("Coffee with $sugarCount spoon of sugar for $name")
+    }
 }
